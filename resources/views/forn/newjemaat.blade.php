a6330937fa616fb2bcd000c6f7f9153a348a08f5
jepris <jepri.h.simbolon@gmail.com>
2024-08-09 10:00:45 +0700

    formulir

---
+++ resources/views/forn/newjemaat.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form Pendataan Jemaat Baru</title>
+</head>
+<body>
+    <div class="container">
+    <form action="">
+        <div class="mb-3">
+            <label for="nama" class="form-label fw-bold">Nama</label>
+            <input type="text" class="form-control" id="nama" placeholder="nama lengkap"  name="nama" required>
+        </div>
+        <div class="mb-3">
+            <label for="no_hp" class="form-label fw-bold">No. Handphone</label>
+            <input type="number" class="form-control" id="no_hp" placeholder="08xxxxxxxxxx"  name="no_hp" required>
+        </div>
+        <div class="mb-3">
+            <label for="alamat" class="form-label fw-bold">alamat</label>
+            <input type="text" class="form-control" id="alamat" placeholder="jalan --"  name="alamat"  required>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label fw-bold">Email address</label>
+            <input type="email" class="form-control" id="email"
+                placeholder="name@example.com"  name="email" required>
+        </div>
+        <div class="mb-3">
+            <label for="gereja_asal" class="form-label fw-bold">Gereja Asal</label>
+            <input type="text" class="form-control" id="gereja_asal"
+                placeholder="Hkbp --"  name="gereja_asal" required>
+        </div>
+        <div class="mb-3">
+            <label for="file" class="form-label">Upload Surat Referensi Gereja Asal</label>
+            <input class="file" type="file" id="file" accept=".pdf"  name="file" required>
+          </div>
+        <div class="mb-3">
+            <label for="keluarga" class="form-label fw-bold">Anggota Keluarga</label>
+            <textarea class="form-control" id="keluarga" rows="4"  name="keluarga"  required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Kirim</button>
+    </form>
+</div>
+</body>
+</html>
--- resources/views/forn/sakit.blade.php
+++ resources/views/forn/sakit.blade.php
@@ -5,15 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form Kontak</title>
+    <title>Form Pendataan Jemaat Sakit</title>
 </head>
 
 <body>
     <div class="container">
         <form action="">
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label fw-bold">Nama</label>
+                <label for="exampleFormControlInput1" class="form-label fw-bold">Nama / Keluarga</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="nama lengkap">
+            </div>
+            <div class="mb-3">
+                <label for="wijk" class="form-label fw-bold">Wijk</label>
+                <select class="form-select" name="wijk" aria-label="Default select example" id="wijk">
+                    @for ($i = 1; $i <= 20; $i++)
+                        <option value="{{ $i }}">{{ $i }}</option>
+                    @endfor
+                </select>
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label fw-bold">No. Handphone</label>
@@ -25,16 +33,12 @@
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label fw-bold">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <input type="email" class="form-control" id="exampleFormControlInput1"
+                    placeholder="name@example.com">
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label fw-bold">Perihal</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1"
-                    placeholder="Jamuan, Layanan Doa, ..">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label fw-bold">Rincian</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+                <label for="exampleFormControlTextarea1" class="form-label fw-bold">Info Anggota Keluarga yang Sakit</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Nama/Gender/Alamat rumah | Rumah Sakit + No.kamar"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Kirim</button>
         </form>
