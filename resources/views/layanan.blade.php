d589f363b6cb524c9ddbb0a86110be32d854f48e
jepris <jepri.h.simbolon@gmail.com>
2024-08-09 09:41:51 +0700

    tampilan

--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -6,37 +6,43 @@
         <section>
             <div class="layanan">
                 <div class="layanan-isi">
-                    <h4 class="fw-bold " style="text-decoration: underline; color: #0127Ff;">Layanan Jemaat HKBP Perumnas Batu Onom Online
+                    <h4 class="fw-bold " style="text-decoration: underline; color: #0127Ff;">Layanan Jemaat HKBP Perumnas Batu
+                        Onom Online
                     </h4>
                     <p>Untuk dapat meningkatkan pelayanan terhadap jemaat, HKBP Perumnas Batu Onom membuka jalur
                         pelayanan
                         via
                         online. Silahkan klik link di bawah sesuai dengan layanan yang diinginkan</p>
                     <ul>
-                        <li class="mb-3">Pendaftaran Jemaat Baru <button type="button" class="btn btn-primary" ><a href="/hurias/newjemaat">
-                            disini . . .</a>
-                          </button></li>
-                        <li class="mb-3">Pemberitahuan Anak Lahir <button type="button" class="btn btn-primary" ><a href="/hurias/newbaby">
-                          disini . . .</a>
-                          </button></li>
-                        <li class="mb-3">Permohonan Baptisan <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#baptis">
-                            disini . . .
-                          </button></li>
-                        <li class="mb-3">Pendaftaran Sidi/Katekisasi <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#sidi">
-                            disini . . .
-                          </button></li>
-                        <li class="mb-3">Pendaftaran Pernikahan <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#nikah">
-                            disini . . .
-                          </button></li>
-                        <li class="mb-3">Pemberitahuan jemaat Sakit <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#sakit">
-                            disini . . .
-                          </button></li>
-                        <li class="mb-3">Permohonan Pindah Jemaat/Wijk <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pindah">
-                            disini . . .
-                          </button></li>
-                        <li class="mb-3">Pemberitahuan Jemaat Meninggal Dunia <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#meninggal">
-                            disini . . .
-                          </button></li>
+                        <a href=""></a> disini . . .</a>
+                        </button></li>
+                        <li class="mb-3">Pemberitahuan Anak Lahir <a type="button" class="btn btn-primary"><a
+                                    href="/hurias/newbaby">
+                                    disini . . .</a>
+                            </a></li>
+                        <li class="mb-3">Permohonan Baptisan <a href="/">
+                                disini . . .
+                            </a></li>
+                        <li class="mb-3">Pendaftaran Sidi/Katekisasi <button type="button" class="btn btn-primary"
+                                data-bs-toggle="modal" data-bs-target="#sidi">
+                                disini . . .
+                            </button></li>
+                        <li class="mb-3">Pendaftaran Pernikahan <button type="button" class="btn btn-primary"
+                                data-bs-toggle="modal" data-bs-target="#nikah">
+                                disini . . .
+                            </button></li>
+                        <li class="mb-3">Pemberitahuan jemaat Sakit <button type="button" class="btn btn-primary"
+                                data-bs-toggle="modal" data-bs-target="#sakit">
+                                disini . . .
+                            </button></li>
+                        <li class="mb-3">Permohonan Pindah Jemaat/Wijk <button type="button" class="btn btn-primary"
+                                data-bs-toggle="modal" data-bs-target="#pindah">
+                                disini . . .
+                            </button></li>
+                        <li class="mb-3">Pemberitahuan Jemaat Meninggal Dunia <button type="button"
+                                class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#meninggal">
+                                disini . . .
+                            </button></li>
                     </ul>
                 </div>
             </div>
