b0eec320206275436e7c69beed349c298f346170
reynaldi15 <reynaldisilalahi09@gmail.com>
2024-08-10 16:11:50 +0700

    update admin fitur

---
+++ resources/views/admin/internal/wartameninggal.blade.php
@@ -0,0 +1,40 @@
+@extends('admin.utils.aside')
+@section('content')
+
+<section class="content">
+    <div class="container-fluid">
+        <h3 class="mt-3 fw-bold">Data Jemaat yang Meninggal Setiap Minggu</h3>
+        <div class="data-jemaat-dashboard" style="overflow-x: auto; max-height: 200px; overflow-y: auto;">
+            <table class="table table-bordered">
+                <thead class="sticky-top">
+                    <tr class="text-center">
+                        <th scope="col">No.</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">wijk</th>
+                            <th scope="col">No Hp </th>
+                            <th scope="col">Alamat </th>
+                            <th scope="col">Email </th>
+                            <th scope="col">keterangan </th>
+                            <th scope="col">Tanggal Dibuat </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($wartameninggal as $data)
+                        <tr>
+                            <td scope="row" class="text-center">{{ $loop->iteration }}</td>
+                            <td>{{ $data->name }}</td>
+                            <td>{{ $data->wijk }}</td>
+                            <td>{{ $data->notelp }}</td>
+                            <td>{{ $data->alamat }}</td>
+                            <td>{{ $data->email }}</td>
+                            <td>{{ $data->keterangan }}</td>
+                            <td>{{ $data->created_at->format('d-m-Y H:i') }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,12 @@ use App\Models\Warta;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\NewsController;
-use App\Http\Controllers\Admin\HuriaController;
-use App\Http\Controllers\Admin\LahirController;
 use App\Http\Controllers\Admin\WartaController;
 use App\Http\Controllers\Admin\GaleriController;
 use App\Http\Controllers\Admin\JadwalController;
 use App\Http\Controllers\Admin\JemaatController;
 use App\Http\Controllers\Admin\KeuanganController;
+use App\Http\Controllers\Admin\LayananController;
 use App\Http\Controllers\Admin\ScheduleController;
 use App\Http\Controllers\CreateController;
 use App\Models\Huria;
@@ -28,6 +27,7 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 */
 
 // Bagian User route
+// get function 
 Route::get('/', [DashboardController::class, 'index']);
 Route::get('/beranda', [DashboardController::class, 'index'])->name('beranda');
 Route::get('/warta-jemaat', [DashboardController::class, 'wartajemaat']);
@@ -36,7 +36,7 @@ Route::get('/image', [DashboardController::class, 'image']);
 Route::get('/tentang', [DashboardController::class, 'tentang']);
 Route::get('/layanan', [DashboardController::class, 'layanan']);
 Route::get('/hurias', [CreateController::class, 'index'])->name('hurias.index');
-Route::get('/tentang', [CreateController::class, 'tentang'])->name('tentangg.tentang');
+Route::get('/tentang', [CreateController::class, 'tentang'])->name('tentang.tentang');
 Route::get('/hurias/newkontak', [CreateController::class, 'createkontak'])->name('createkontak');
 Route::get('/hurias/newbaptis', [CreateController::class, 'createbaptis'])->name('createbaptis');
 Route::get('/hurias/newlahir', [CreateController::class, 'createlahir'])->name('createlahir');
@@ -45,8 +45,7 @@ Route::get('/hurias/newnikah', [CreateController::class, 'createnikah'])->name('
 Route::get('/hurias/newsakit', [CreateController::class, 'createsakit'])->name('createsakit');
 Route::get('/hurias/newpindah', [CreateController::class, 'createpindah'])->name('createpindah');
 Route::get('/hurias/newmeninggal', [CreateController::class, 'createmeninggal'])->name('createmeninggal');
-
-
+// post function
 Route::post('/hurias/newlahir', [CreateController::class, 'storelahir'])->name('hurias.storelahir');
 Route::post('/hurias/newbaptis', [CreateController::class, 'storebaptis'])->name('hurias.storebaptis');
 Route::post('/hurias/newsidi', [CreateController::class, 'storesidi'])->name('hurias.storesidi');
@@ -67,6 +66,38 @@ Route::get('/dashboard',[JemaatController::class, 'dashboard'])->name('dashboard
 Route::resource('jemaat',JemaatController::class);
 Route::resource('keuangan',KeuanganController::class);
 Route::get('/birthday',[JemaatController::class, 'birthday'])->name('birthday');
+
+// Route::get('/birthday',[JemaatController::class, 'birthday'])->name('birthday');
+
+Route::get('/lahirs',[LayananController::class, 'indexlahir'])->name('lahirs.indexlahir');
+Route::get('/pindahs',[LayananController::class, 'indexpindah'])->name('lahirs.indexpindah');
+Route::get('/baptis',[LayananController::class, 'indexbaptis'])->name('baptis.indexbaptis');
+Route::get('/sidis',[LayananController::class, 'indexsidi'])->name('sidis.indexsidi');
+Route::get('/nikahs',[LayananController::class, 'indexnikah'])->name('nikahs.indexnikah');
+Route::get('/sakits',[LayananController::class, 'indexsakit'])->name('sakits.indexsakit');
+Route::get('/meninggals',[LayananController::class, 'indexmeninggal'])->name('meninggals.indexmeninggal');
+Route::get('/wartasakit',[LayananController::class, 'wartasakit'])->name('wartasakit');
+Route::get('/wartameninggal',[LayananController::class, 'wartameninggal'])->name('wartameninggal');
+Route::get('/wartanikah',[LayananController::class, 'wartanikah'])->name('wartanikah');
+
+Route::delete('/lahirs/{id}', [LayananController::class, 'destroylahir'])->name('lahirs.destroylahir');
+Route::delete('/pindahs/{id}', [LayananController::class, 'destroypindah'])->name('pindahs.destroypindah');
+Route::delete('/baptis/{id}', [LayananController::class, 'destroybaptis'])->name('baptis.destroybaptis');
+Route::delete('/sidis/{id}', [LayananController::class, 'destroysidi'])->name('sidis.destroysidi');
+Route::delete('/nikahs/{id}', [LayananController::class, 'destroynikah'])->name('nikahs.destroynikah');
+Route::delete('/sakits/{id}', [LayananController::class, 'destroysakit'])->name('sakits.destroysakit');
+Route::delete('/meninggals/{id}', [LayananController::class, 'destroymeninggal'])->name('meninggals.destroymeninggal');
+
+Route::put('/lahirs/{id}', [LayananController::class, 'updatelahir'])->name('lahirs.updatelahir');
+Route::put('/pindah/{id}', [LayananController::class, 'updatepindah'])->name('pindahs.updatepindah');
+Route::put('/baptis/{id}', [LayananController::class, 'updatebaptis'])->name('baptis.updatebaptis');
+Route::put('/sidis/{id}', [LayananController::class, 'updatesidi'])->name('sidis.updatesidi');
+Route::put('/nikahs/{id}', [LayananController::class, 'updatenikah'])->name('nikahs.updatenikah');
+Route::put('/sakits/{id}', [LayananController::class, 'updatesakit'])->name('sakits.updatesakit');
+Route::put('/meninggals/{id}', [LayananController::class, 'updatemeninggal'])->name('meninggals.updatemeninggal');
+
+
+
 
 route::get('/login', function(){
     return view('auth.login');
